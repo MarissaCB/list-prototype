4781bcf6db06961ffa0b2fcebc5c4727ba232dcc
MarissaCB <marissacody@gmail.com>
2016-07-29 10:56:40 -0400

    jquery append

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-	$('#button').on('click', function() {
+	
+	/*$('#button').on('click', function() {
 	 var value = $("#inputbox").val ()
-	})
+	 */
+	 $("#add_li").click(function () {
+		$("ol").append("<li></li>");
+	 });
+
+	 $("#remove_li").click(function () {
+		$("ol").remove("<li></li>");
+	 }); 
+	
 });
