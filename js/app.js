4b4b6a66c24780e6a2cc92814c84f6e940b03399
MarissaCB <marissacody@gmail.com>
2016-07-29 12:29:43 -0400

    jquery input

--- js/app.js
+++ js/app.js
@@ -3,8 +3,11 @@ $(document).ready(function() {
 	/*$('#button').on('click', function() {
 	 var value = $("#inputbox").val ()
 	 */
-	 $("#add_li").click(function () {
-		$("ol").append("<li></li>");
+
+	 $("#add_li").on("click", function() {
+	 	var value = $("#inputBox").val();
+	 	console.log(value);
+		$("ol").append("<li>" + value + " <input type='checkbox' class='remove-box'></li>");
 	 });
 
 	 $("#remove_li").click(function () {
